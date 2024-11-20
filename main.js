6334c63cc39b2c7ae79b0fede8bc8797fb00cf0c
solider1385 <109354084+solider1385@users.noreply.github.com>
2024-11-21 02:08:50 +0330

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+
+show_8540818().then(() => {
+    // You need to add your user reward function here, which will be executed after the user watches the ad.
+    // For more details, please refer to the detailed instructions.
+    alert('You have seen ad ad!');
+})
+        
